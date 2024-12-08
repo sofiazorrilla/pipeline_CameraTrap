4b77a2283d23d5117cf43dbd5686fee530fee5c3
Sofía Zorrilla <sofia.zorrilla95@gmail.com>
2024-12-08 14:51:32 -0600

    Agregue el folder de bin y un script de prueba para extraer metadatos de las imagenes

---
+++ bin/extract_metadata.R
@@ -0,0 +1,13 @@
+######
+# Script : title
+# Author: Sofía Zorrilla and César Díaz
+# Date: 2024-12-08
+# Description: Extraction of metadata from image folders
+# Arguments:
+#   - Input: 
+#   - Output: 
+#######
+
+# --- Load packages ---
+
+library(cameratrapR)
